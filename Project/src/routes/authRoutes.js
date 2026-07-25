301f079c4b6a634c1a1478fa3ac3a6723843d431
= <=>
2026-07-25 15:34:57 -0300

    Nova tentativa

--- Project/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController.js";
-
-const authRoutes = Router();
-
-authRoutes.post('/login', authController.login);
-
-export default authRoutes;
